e844d322fdde4c3962784094f85c97b84a965f50
Rachel Yehezkel <rachelyehe@hotmail.com>
2015-11-15 11:20:06 +0200

    changes

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ var http = require('http');
 // Configure our HTTP server to respond with Hello World to all requests.
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello World\n");
+  response.end("Hello IGTCloud\n");
 });
 
-// Listen on port 8000, IP defaults to 127.0.0.1
+// Listen on port 8000
 server.listen(8000);
 
 // Put a friendly message on the terminal
-console.log("Web Server run on http://127.0.0.1:8000");
+console.log("Web Server running on port 8000");
